c29d987faac29a70d367d0c532f6308f6bdccb86
agil6778 <vanessalarizze@gmail.com>
2017-02-06 17:49:48 -0200

    Coloquei uma exclamação

--- TesteGit.java
+++ TesteGit.java
@@ -1,5 +1,5 @@
 public class TesteGit{
 	public static void main(String [] args){
-		System.out.println("Exemplo de teste de uso do git");
+		System.out.println("Exemplo de teste de uso do git!");
 	}
 }
